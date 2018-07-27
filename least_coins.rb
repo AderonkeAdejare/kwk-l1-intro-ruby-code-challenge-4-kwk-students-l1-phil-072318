70a2ab1993f5051a9070c8de97770015697432dd
AderonkeAdejare <aderonkea@comcast.net>
2018-07-27 12:57:32 +0000

    automatically backed up by learn

--- least_coins.rb
+++ least_coins.rb
@@ -1,7 +1,7 @@
-#write out your code here
-
 def least_coins(cents)
-
-#Code your answer here!
-
+  cents = {:quarters => 0.25, :dimes => 0.10, :nickels => 0.05, :pennies => 0.01 }
 end
+
+puts "How much change do you need?"
+change = gets.chomp.to_i
+puts "#{least_coins(change)}"
